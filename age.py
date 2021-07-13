b4305d0c69a85af06cccb4004087e4c5b9026618
rumihsieh <87258074+rumihsieh@users.noreply.github.com>
2021-07-13 15:24:47 +0800

    Add files via upload

--- age.py
+++ age.py
@@ -1,10 +1,13 @@
-age = imput('請輸入你的年齡：')
+car = input('請問你有沒有開車：')
+age = input('請輸入你的年齡：')
 age = int(age)
-if age < 13:
-	print('小學')
-elif age >= 13 and < 18:
-	print('國高中')
-elif age >= 18 and < 22:
-	print('大學生')
-else
-	print('社會人士！')
+if car == '有':
+	if age >= 18:
+		print('你通過測驗了')
+	else:
+		print('奇怪 你怎麼開過車')
+elif car == '沒有':
+	if age >= 18:
+		print('你可以考駕照了啊')
+	else:
+		print('乖寶寶！繼續保持')
